58e587c8daed8cdcda1da2929b8368025d5b29a2
Arth Tyagi <arthtyagi7@gmail.com>
2020-10-18 03:45:28 +0530

    removed django support app to add it again

--- support/support/forum/admin.py
+++
@@ -1,7 +0,0 @@
-from forum.models import Query
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Query)
-admin.site.register(Answer)
-admin.site.register(Comment)
--- support/support/support/mixins.py
+++
@@ -1,9 +0,0 @@
-class PageTitleMixin(object):
-    def get_page_title(self, context):
-        return getattr(self, "title", "Default Page Title")
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["title"] = self.get_page_title(context)
-
-        return context
